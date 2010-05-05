b1ca10bd9dee3e15e2f029ef3b5ffdbf38655773
root <root@abba.mail.info>
2010-05-05 14:07:34 +0100

    intial slightly modified version to suit axlr8 a bit better...

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  `pureftp`.`logins` (
+	`id` INT NOT NULL AUTO_INCREMENT ,
+	`uid` INT NOT NULL ,
+	`gid` INT NOT NULL ,
+	`email` VARCHAR( 255 ) NULL ,
+	`password` VARCHAR( 255 ) NOT NULL ,
+	`username` VARCHAR( 255 ) NOT NULL ,
+	`dir`	VARCHAR( 255 ) NOT NULL,
+	PRIMARY KEY (  `id` )
+) ENGINE = INNODB COMMENT =  'pureuserftp thing'
