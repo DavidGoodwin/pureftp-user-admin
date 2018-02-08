33abbca876495d0117b34218fc759b0565c1971c
David Goodwin <david@palepurple.co.uk>
2018-02-08 22:18:13 +0000

    update schema.sql (drop id, add pk on username etc)

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `logins` (
-	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR( 255 ) NOT NULL ,
 	`uid` INT NOT NULL ,
 	`gid` INT NOT NULL ,
 	`email` VARCHAR( 255 ) NULL ,
 	`password` VARCHAR( 255 ) NOT NULL ,
-	`username` VARCHAR( 255 ) NOT NULL ,
 	`dir`	VARCHAR( 255 ) NOT NULL,
-	PRIMARY KEY (  `id` )
+	PRIMARY KEY ( `username` )
 ) ENGINE = INNODB COMMENT =  'pure-user-ftpadmin'
