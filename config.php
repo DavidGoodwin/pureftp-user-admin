e843594d8da54e0c2673506a4626b5f10337a309
David Goodwin <david@palepurple.co.uk>
2018-04-20 22:21:56 +0100

    update config

--- config.php
+++ config.php
@@ -1,45 +1,40 @@
 <?php
 /**
  * Settings file for PureUserAdmin
- * @see PureUserClass::__construct() which require's this in.
  */
 
-$config = array();
-$config["version"]     = "0.4.0";
-$config["homepage"]    = "https://github.com/DavidGoodwin/pureftp-user-admin";
-// 0 = disabled, 1 = enabled - check if user has read/write access in homedir
-$config["check_access"]= "1"; 
-// 0 = disabled, 1 = enabled - email user with password etc. Database needs field "email"
-$config["notify_user"] = "1"; 
-$config["admin_email"] = "some.one.to.email@example.com";
-$config["ftp_hostname"]= "your.host.name";
+$config = [
+    'version' => '0.4.0',
+    'homepage' => "https://github.com/DavidGoodwin/pureftp-user-admin",
+    'check_access' => true , // boolean - check if user has read/write access in homedir
+    'notify_user' => false, // if enabled, email new user with password. Database needs field "email"
+    'admin_email' => 'admin+pureftp@example.com',
+    'ftp_hostname' => php_uname('n'),
 
 // database settigs 
 // We require a PDO DSN.
-
-$config['database_dsn'] = "mysql:host=localhost;dbname=pureftp";
-$config['database_user'] = 'db_username';
-$config['database_pass'] = 'db_password';
-
-$config["sql_table"]   = "logins";
-
-$config["field_uid"]   = "uid";
-$config["field_gid"]   = "gid";
-$config["field_pass"]  = "password";
-$config["field_user"]  = "username";
-$config["field_dir"]   = "dir";
-$config["field_email"]   = "email";
-
+    'database_dsn' => "mysql:host=localhost;dbname=pureftp",
+    'database_user' => 'db_username',
+    'database_pass' => 'db_password', 
+    'sql_table' => 'logins',
+    'field_uid' => 'uid',
+    'field_gid' => 'gid',
+    'field_pass' => 'password',
+    'field_user' => 'username',
+    'field_dir' => 'dir',
+    'field_email' => 'emali',
+    
 // How we encrypt user's passwords.
-// md5 vs cleartext vs password (for mysql's password() function)
-$config["pwcrypt"]     = "md5"; 
+// crypt vs md5 vs cleartext 
+    'pwcrypt'     => "md5",
+    'default_uid' => "65534", // nobody
+    'default_gid'  => "65534", // nogrop
+    'page_size' => 40,
+];
 
-// we use nobody (on OpenBSD)
-$config["default_uid"] = "65534";		 
-// we use nobody	(on OpenBSD)
-$config["default_gid"] = "65534";
-
-// number of records on 1 page in userlist
-$config["page_size"]   = "40";        
+if(file_exists(dirname(__FILE__) . '/config.local.php')) {
+    // put your local config changes in this file to overwrite $config['things']
+    require_once(dirname(__FILE__) . '/config.local.php');
+}
 
 return $config;
